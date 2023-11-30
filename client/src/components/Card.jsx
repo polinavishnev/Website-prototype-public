a08c830640d491d8826a8d8b8d161f4dd1acf0f9
polinavishnev <polina@uni.minerva.edu>
2023-11-30 13:55:19 -0800

    Separation for deployment

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from 'react';
+import Chat from './Chat';
+
+const Card = ({ question, topic, defaultAnswer, defaultFeedback, sendScoreToParent, childTopic }) => {
+    const [answer, setAnswer] = useState(defaultAnswer);
+    const [feedback, setFeedback] = useState(defaultFeedback);
+    const [loading, setLoading] = useState(false);
+    const [score, setScore] = useState(0);
+    const [chat, setChat] = useState(false);
+    const [dataFromPinecone, setDataFromPinecone] = useState("");
+    const [feedbackPrompt, setFeedbackPrompt] = useState("");
+
+
+    const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+    
+    const handleSendDataToParent = () => {
+      score ? sendScoreToParent(score) : sendScoreToParent(0);
+    }
+    
+    // based on this question, response, and topic, find the relevant information from pinecone
+    const getPineconeSearch = async () => {
+      try {
+        // Fetch data from Pinecone API and update dataFromPinecone state
+        const response = await fetch(`http://localhost:3001/search?text=${encodeURIComponent(question + answer + topic)}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        const dataReceived = await response.json();
+  
+        const concatenatedContent = dataReceived
+          .filter(item => item.pageContent)
+          .map(item => item.pageContent.trim().substring(0, 500))
+          .join('... ');
+  
+        // Set dataFromPinecone state after receiving data
+        setDataFromPinecone(concatenatedContent);
+        console.log(concatenatedContent)
+        return concatenatedContent;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+
+    const getFeedback = async () => {
+      setLoading(true);
+    
+      try {
+        const pineconeData = await getPineconeSearch();
+        // set an artificial delay after await getPineconeSearch()
+            
+        const newFeedbackPrompt = `
+          I answered the following question: ${question}. The question was about this topic:${childTopic.topic ? childTopic.topic : 'Unknown'}.
+          I wrote: ${answer}
+          Here is some contextual information about the question and answer: ${pineconeData}
+          Please provide feedback on my response based on the contextual information, the question, and the topic.
+
+          If I wasn't sure or did not know the answer, please provide feedback on how they could have approached the question.
+          Be kind, supportive, and encouraging. Your role is to make sure that I want to try again in the future and keep on learning. 
+          Please encourage me to keep learning and congratulate me on my effort regardless of my response.
+        `;
+
+        console.log(newFeedbackPrompt)
+    
+        // Make the API call directly with the constructed feedbackPrompt
+        const apiRequestBody = {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            { role: 'system', content: newFeedbackPrompt },
+          ],
+          temperature: 1.2
+        };
+    
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${REACT_APP_API_KEY}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(apiRequestBody),
+        });
+    
+        const data = await response.json();
+        setFeedback(data.choices[0].message.content);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    
+  
+  const handleScore = (value) => {
+      setScore(value);
+  }
+
+  const activateChat = () => {
+    setChat(true);
+  }
+
+  useEffect(() => {
+    setAnswer(defaultAnswer);
+    setFeedback(defaultFeedback);
+    setScore(0);
+  }, [question])
+
+    
+  useEffect( () => {
+    console.log(topic, 'topic within card')
+    handleSendDataToParent();
+  }, [feedback])
+
+
+  // use useeffect to set chat to empty when the question changes
+  useEffect(() => {
+    setChat(false);
+  } , [question, feedback])
+
+  useEffect(() => {
+    handleSendDataToParent();
+}, [score]);
+
+  // use useeffect to set chat to empty when the question changes
+  useEffect(() => {
+    setChat(false);
+  } , [question, feedback])
+
+    return (
+        <div className="card">
+            <h2>Question: {question}</h2>
+            <textarea
+                type="text"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                placeholder="What do you think is the answer?">
+            </textarea>
+            {loading ? null : 
+            <button className="get-feedback-button" onClick={() => getFeedback(topic)}>Get feedback</button>}
+            <div className="answer-box">
+                {loading ? "Loading..." : (
+                  <>
+                    {feedback}
+                  
+                  </>
+                )}
+                {feedback && (
+                      <>
+                        <br/>
+                        <button className="discuss-button" onClick={() => activateChat()}>
+                          I want to discuss feedback
+                        </button>
+                      </>
+                    )}
+                {chat && (
+                  <Chat
+                    question={question}
+                    answer={answer}
+                    feedback={feedback}
+                    childTopic={childTopic}
+                  />
+                )}
+
+              {feedback && (
+                          <div className="self-eval-box">
+                            <h3>Now that you understand what was expected, how do you think you did?</h3>
+
+                            <div className='self-evaluation'>
+                                <button className="eval-button" onClick={() => handleScore(2)}>I got it correct</button>
+                                <button className="eval-button" onClick={() => handleScore(1)}>I got it partially correct</button>
+                                <button className="eval-button" onClick={() => handleScore(0)}>I got it wrong</button>
+                                <button className="eval-button" onClick={() => handleScore(0)}>I did not know the answer</button>
+                            </div>
+                            </div>
+                      )}
+
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ client/src/components/Chat.jsx
@@ -0,0 +1,129 @@
+import { useState, useEffect }  from 'react';
+import {
+    MainContainer,
+    ChatContainer,
+    MessageList,
+    Message,
+    MessageInput,
+    TypingIndicator,
+  } from '@chatscope/chat-ui-kit-react';
+
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+
+
+    const Chat = ( {question, answer, feedback, topic} ) => {
+        const [messages, setMessages] = useState([
+            {
+                message: `I am a student trying to learn. I was asked the question ${question} about ${topic} and I answered ${answer}.
+                The feedback I got was ${feedback}. 
+                I would like to discuss this feedback further.`,
+                sentTime: "just now",
+                sender: "user",
+            },
+            {
+                message: `I am a supportive teacher who helps students understand the feedback they receive.
+                                Ask me anything!`,
+                sentTime: "just now",
+                sender: "ChatGPT",
+            },
+        ]);
+        const [isTyping, setIsTyping] = useState(false);
+    
+        const handleSendRequest = async (message) => {
+            const newMessage = {
+                message,
+                direction: 'outgoing',
+                sender: "user",
+            };
+
+    
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
+            setIsTyping(true);
+    
+            try {
+                const response = await processMessageToChatGPT([...messages, newMessage]);
+                const content = response.choices[0]?.message?.content;
+                if (content) {
+                    const chatGPTResponse = {
+                        message: content,
+                        sender: "ChatGPT",
+                    };
+                    setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+                    console.log(messages)
+                }
+            } catch (error) {
+                console.error("Error processing message:", error);
+            } finally {
+                setIsTyping(false);
+            }
+        };
+    
+        async function processMessageToChatGPT(chatMessages) {
+            const apiMessages = chatMessages.map((messageObject) => {
+                const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
+                return { role, content: messageObject.message };
+            });
+    
+            const apiRequestBody = {
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    { role: "system", content: 
+                        `I'm a Student using this for learning. 
+                        ` },
+                    ...apiMessages,
+                ],
+            };
+    
+            const response = await fetch("https://api.openai.com/v1/chat/completions", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Bearer " + API_KEY,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(apiRequestBody),
+            });
+    
+            return response.json();
+        }
+    
+        return (
+            <div className="Chat">
+                <div style={{ display: "flex" }}>
+                    <MainContainer>
+                        <ChatContainer>       
+                            <MessageList 
+                                scrollBehavior="smooth" 
+                                typingIndicator={isTyping ? <TypingIndicator content="Assistant is typing" /> : null}
+                            >   <h2>Chat</h2>
+                                {messages.slice(1).map((message, i) => {
+                                    console.log(message)
+                                    return (
+                                        <div style={{display: "flex", flexDirection: "column"}}>
+                                            <Message
+                                                key={i}
+                                                model={message} 
+                                                isOwn={message.sender === "user"} 
+                                                style={{ 
+                                                    backgroundColor: message.sender === "user" ? "#333" : "#000", 
+                                                    color: "#fff",
+                                                    alignSelf: message.sender === "user" ? "flex-end" : "flex-start",
+                                                    textAlign: message.sender === "user" ? "right" : "left",
+                                                    margin: "0.5em"
+                                                }}
+                                            />
+                                        </div>
+                                    );
+                                })}
+                            </MessageList>
+                            <MessageInput placeholder="Send a Message" onSend={handleSendRequest} />        
+                        </ChatContainer>
+                    </MainContainer>
+                </div>
+            </div>
+        )
+    }
+
+  
+  export default Chat;
