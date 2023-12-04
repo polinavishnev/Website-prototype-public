d117024acdaacde9ecb28333228cf6f4e513fae1
Saad Bin Ihsan <saadihsan287@gmail.com>
2023-12-03 16:21:31 -0800

    Added npm packages

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,81 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Upload({ onUploadFinish }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [uploading, setUploading] = useState(false);
 
   const handleTextSubmit = async (e) => {
     e.preventDefault();
-    if (text.trim() !== '') {
+    if (text.trim() !== "") {
       setUploading(true);
-  
+
       try {
         // Upload text to your server
         const croppedText = text.substring(0, 7000);
         const prompt = `Based on this text, generate a list of 3 specific but overarching topics that would be relevant to be quizzed on in relation to this text. \n\nText: ${croppedText}`;
-  
-        const response = await fetch('http://localhost:3001/upload', {
-          method: 'POST',
+
+        const response = await fetch("http://localhost:3001/upload", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ text }),
         });
-  
+
         if (response.ok) {
-          console.log('Text uploaded successfully');
-  
+          console.log("Text uploaded successfully");
+
           // Generate topics using OpenAI API after text upload
           const apiRequestBody = {
-            model: 'gpt-3.5-turbo',
+            model: "gpt-3.5-turbo",
             messages: [
-              { role: 'system', content: prompt }, // Use the uploaded text here
+              { role: "system", content: prompt }, // Use the uploaded text here
             ],
             temperature: 0.8,
           };
-  
-          const aiResponse = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-              Authorization: `Bearer ${API_KEY}`,
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(apiRequestBody),
-          });
-  
+
+          const aiResponse = await fetch(
+            "https://api.openai.com/v1/chat/completions",
+            {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(apiRequestBody),
+            }
+          );
+
           if (aiResponse.ok) {
             const aiData = await aiResponse.json();
-            console.log('AI Response:', aiData);
-  
+            console.log("AI Response:", aiData);
+
             // Extract topics from AI response (adjust this logic based on API response structure)
             const extractedTopics = aiData.choices[0].message.content
-                .split('\n')
-                .filter(line => line.trim().length > 0) // Filter out empty lines
-                .map(line => line.trim()) 
-  
+              .split("\n")
+              .filter((line) => line.trim().length > 0) // Filter out empty lines
+              .map((line) => line.trim());
+
             // Call the callback function with the extracted topics
-            console.log('Extracted topics:', extractedTopics)
+            console.log("Extracted topics:", extractedTopics);
             onUploadFinish(extractedTopics);
           } else {
-            console.error('Error fetching topics from AI API');
+            console.error("Error fetching topics from AI API");
             // Handle error fetching topics from the AI API
           }
         } else {
           const errorMessage = await response.text(); // Get the error message
-          console.error('Error uploading text:', errorMessage);
+          console.error("Error uploading text:", errorMessage);
           // Handle error uploading text
         }
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
         // Handle general error scenario
       }
-  
+
       setUploading(false);
     }
   };
-  
 
   const handleTextChange = (e) => {
     setText(e.target.value);
@@ -97,9 +99,16 @@ function Upload({ onUploadFinish }) {
         ></textarea>
         <br />
         <button type="submit" disabled={uploading}>
-          {uploading ? 'Uploading...' : 'Upload'}
+          {uploading ? "Uploading..." : "Upload"}
         </button>
       </form>
+      <button
+        style={{
+          marginTop: "3px",
+        }}
+      >
+        New Text
+      </button>
     </div>
   );
 }
